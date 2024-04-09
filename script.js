f5b5727679ffa5bec6c8bbfb71320ecc20c7d63c
Tutorial <tpanagopulos2@gmail.com>
2024-04-09 15:57:02 +0200

    Changing css/. Adding variants for CV (english ,czech) interactive buttons,etc

--- script.js
+++ script.js
@@ -5,3 +5,16 @@ function toggleMenu() {
   menu.classList.toggle('open')
   icon.classList.toggle('open');
 }
+
+const cvButtonEl = document.querySelector('.js-button-cv');
+const langButtonEl = document.querySelector('.js-options-container');
+
+cvButtonEl.addEventListener('click', () => {
+  if(langButtonEl.classList.contains('options-container')) { // Check if langButtonEl has the class 'options-container'
+    langButtonEl.classList.remove('options-container');
+    langButtonEl.classList.add('cv-visible');
+  } else if(langButtonEl.classList.contains('cv-visible')) { // Check if langButtonEl has the class 'cv-visible'
+    langButtonEl.classList.remove('cv-visible');
+    langButtonEl.classList.add('options-container');
+  }
+});
